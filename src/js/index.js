c29c168854db21350f4522fc5ede97999fd166ad
InTr0p <christianolivare01@gmail.com>
2022-02-14 00:30:38 +0000

    third upload

--- src/js/index.js
+++ src/js/index.js
@@ -4,17 +4,22 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import "bootstrap";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-function SimpleCounter (props){
-    return <div className="Counter">
-        <div className="calendar"></div>
-        <div className="one"></div>
-        <div className="two"></div>
-        <div className="tree"></div>
-        <div className="four"></div>
-    </div>
+function SimpleCounter(props) {
+	return (
+		<div className="Counter">
+			<div className="calendar">
+				<i className="far fa-clock"></i>{" "}
+			</div>
+			<div className="four">{props.digitFour}</div>
+			<div className="tree">{props.digittree}</div>
+			<div className="two">{props.digittwo}</div>
+			<div className="one">{props.digitone}</div>
+		</div>
+	);
 }
-
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
