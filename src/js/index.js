67ccb0440ab09c5cf0ffa1ae40792083b0ef3381
InTr0p <christianolivare01@gmail.com>
2022-02-14 01:08:10 +0000

    six upload

--- src/js/index.js
+++ src/js/index.js
@@ -6,37 +6,44 @@ import propTypes from "prop-types";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 import "bootstrap";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function SimpleCounter(props) {
-    return (
-        <div className="Counter">
-            <div className="calendar">
-                <i className="far fa-clock"></i>
-            </div>
-            <div className="four">{props.digitFour}</div>
-            <div className="tree">{props.digittree}</div>
-            <div className="two">{props.digittwo}</div>
-            <div className="one">{props.digitone}</div>
-        </div>
-    );
+	return (
+		<div className="Counter">
+			<div className="calendar">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="tree">{props.digitThree % 10}</div>
+			<div className="Two">{props.digitTwo % 10}</div>
+			<div className="One">{props.digitOne % 10}</div>
+		</div>
+	);
 }
 SimpleCounter.prototype = {
-    digitFour: propTypes.number,
-    digitthree: propTypes.number,
-    digittwo: propTypes.number,
-    digitone: propTypes.number,
+	digitFour: propTypes.number,
+	digitThree: propTypes.number,
+	digitTwo: propTypes.number,
+	digitOne: propTypes.number,
 };
 
 let Counter = 0;
 setInterval(function () {
-    const four = math.floor(Counter / 1000);
-    const three = math.floor(Counter / 100);
-    const two = math.floor(Counter / 10);
-    const one = math.floor(Counter / 1);
-    console.log(four, three, two, one)
-    //render your react application
-    Counter++;
-    ReactDOM.render(<SimpleCounter digitone={one} digitone={two} digitone={three} digitone={four} />, document.querySelector("#app")
-    );
+	const Four = Math.floor(Counter / 1000);
+	const Three = Math.floor(Counter / 100);
+	const Two = Math.floor(Counter / 10);
+	const One = Math.floor(Counter / 1);
+	console.log(Four, Three, Two, One);
+
+	Counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={One}
+			digitOne={Two}
+			digitOne={Three}
+			digitOne={Four}
+		/>,
+		document.querySelector("#app")
+	);
 }, 1000);
